0bc62369d9f563d1fe086956b4974fe2ad84ab18
Grabner-Haider Julia <julia.grabner-h@outlook.com>
2022-04-13 18:20:40 +0200

    src

--- src/main/java/at/campus02/bsd/Calculator.java
+++ src/main/java/at/campus02/bsd/Calculator.java
@@ -1,2 +1,26 @@
-package at.campus02.bsd;public class Calculator {
+package at.campus02.bsd;
+
+public class Calculator {
+    public double add(double number1, double number2){
+        double result;
+        result = number1 + number2;
+        return result;
+    }
+    public double minus(double number1, double number2){
+        double result;
+        result = number1 - number2;
+        return result;
+    }
+    public double divide(double number1, double number2){
+        double result;
+        result = number1 / number2;
+        return result;
+
+    }
+    public double multiply(double number1, double number2){
+        double result;
+        result = number1 * number2;
+        return result;
+
+    }
 }
--- src/main/java/at/campus02/bsd/Main.java
+++ src/main/java/at/campus02/bsd/Main.java
@@ -1,2 +1,13 @@
-package at.campus02.bsd;public class Main {
+package at.campus02.bsd;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+        System.out.println(calc.add(2,5));
+        System.out.println(calc.minus(8,5));
+        System.out.println(calc.divide(15,2));
+        System.out.println(calc.multiply(15,2));
+
+    }
 }
