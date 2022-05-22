a3cc18cb651cfb156b12257690c5842b072c7409
grabnerjulia13 <s51169@edu.campus02.at>
2022-05-22 18:01:10 +0200

    src/test/java/at/campus02/bsd/SimpleTest.java

--- src/test/java/at/campus02/bsd/SimpleTest.java
+++ src/test/java/at/campus02/bsd/SimpleTest.java
@@ -1,2 +1,30 @@
-package at.campus02.bsd;public class SimpleTest {
+package at.campus02.bsd;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+    public Calculator ca;
+    @BeforeEach
+    public void setUp(){ ca = new Calculator();}
+
+    double result;
+    @Test
+    public void add1(){Assertions.assertEquals(5,result = 3+2);}
+    @Test
+    public void add2(){Assertions.assertEquals(3+2,ca.add(3,2));}
+    @Test
+    public void minus(){Assertions.assertEquals(6,result = 8-2);}
+    @Test
+    public void minus2(){Assertions.assertEquals(8-2,ca.minus(8,2));}
+    @Test
+    public void multi(){Assertions.assertEquals(20,result = 5*4);}
+    @Test
+    public void multi2(){Assertions.assertEquals(5*4,ca.multiply(5,4));}
+    @Test
+    public void divi(){Assertions.assertEquals(4,result = 12/3);}
+    @Test
+    public void divi2(){Assertions.assertEquals(12/3,ca.divide(12,3));}
+
 }
